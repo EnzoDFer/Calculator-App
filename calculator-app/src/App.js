c44891c85875a60e5e6b9ac22daf037f337339ed
EnzoDFer <enzodfer@outlook.com>
2022-07-05 15:45:52 -0400

    Create calculator foundation components and layout

---
+++ calculator-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Interactables from './Interactables';
+import Screen from './Screen';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Screen/>
+      <Interactables/>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator-app/src/Screen.js
@@ -0,0 +1,10 @@
+const Screen = () => {
+  return (
+    <div className="screen">
+      <div className="storage">Previous Number</div>
+      <div className="result">Number + Number =</div>
+    </div>
+  );
+}
+
+export default Screen;
