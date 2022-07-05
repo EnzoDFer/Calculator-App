384f24f8cb445ff543911559ed0f5b94f5709a8a
EnzoDFer <enzodfer@outlook.com>
2022-07-05 16:39:38 -0400

    Add DigitButton component props

--- calculator-app/src/Interactables.js
+++ calculator-app/src/Interactables.js
@@ -3,42 +3,42 @@ import DigitButton from "./DigitButton";
 const Interactables = () => {
   return (
     <div className="interactables">
-        <DigitButton value={'1'}></DigitButton>
-        <DigitButton value={'2'}></DigitButton>
-        <DigitButton value={'3'}></DigitButton>
-        <DigitButton value={'('}></DigitButton>
-        <DigitButton value={')'}></DigitButton>
-        <DigitButton value={'*'}></DigitButton>
-        <DigitButton value={'-'}></DigitButton>
-        <DigitButton value={'1'}></DigitButton>
-        <DigitButton value={'2'}></DigitButton>
-        <DigitButton value={'3'}></DigitButton>
-        <DigitButton value={'4'}></DigitButton>
-        <DigitButton value={'5'}></DigitButton>
-        <DigitButton value={'6'}></DigitButton>
-        <DigitButton value={'7'}></DigitButton>
-        <DigitButton value={'/'}></DigitButton>
-        <DigitButton value={'9'}></DigitButton>
-        <DigitButton value={'10'}></DigitButton>
-        <DigitButton value={'11'}></DigitButton>
-        <DigitButton value={'12'}></DigitButton>
-        <DigitButton value={'13'}></DigitButton>
-        <DigitButton value={'14'}></DigitButton>
-        <DigitButton value={'15'}></DigitButton>
-        <DigitButton value={'16'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'1'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'2'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'3'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'('}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={')'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'*'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'-'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'1'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'2'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'3'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'4'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'5'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'6'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'7'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'/'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'9'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'10'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'11'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'12'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'13'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'14'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'15'}></DigitButton>
+        <DigitButton onClick={(value)=>alert(value)} value={'16'}></DigitButton>
         <div className="digits">
-          <DigitButton value={'7'}></DigitButton>
-          <DigitButton value={'8'}></DigitButton>
-          <DigitButton value={'9'}></DigitButton>
-          <DigitButton value={'4'}></DigitButton>
-          <DigitButton value={'5'}></DigitButton>
-          <DigitButton value={'6'}></DigitButton>
-          <DigitButton value={'1'}></DigitButton>
-          <DigitButton value={'2'}></DigitButton>
-          <DigitButton value={'3'}></DigitButton>
-          <DigitButton value={'0'}></DigitButton>
-          <DigitButton value={'.'}></DigitButton>
-          <DigitButton value={'='}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'7'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'8'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'9'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'4'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'5'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'6'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'1'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'2'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'3'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'0'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'.'}></DigitButton>
+          <DigitButton onClick={(value)=>alert(value)} value={'='}></DigitButton>
         </div>
       </div>
   );
